dcc940017976057b94236f26bef84118f00c3a1a
Joe Pruskowski <joe@callidity.com>
2015-05-29 22:53:17 -0700

    V0.6 - user must now select an answer prior to submit

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,20 +1,22 @@
 $( document ).ready(function() {
-	//alert("ready");
-	
+	/*
+	 * Get the statistics and update the div.
+	 */
 	$("span.reset-link").click(function(){
-		console.log("issuing ajax");
 		$.ajax({url: "statistics", 
 				type: 'POST',
 				data: {action: "reset"},
 				success: function(result) {
 			$("div#statistics").replaceWith(result);
 			if($('table#admin-attempts').length) location.reload(true);
-    		console.log(result);
     		}
     	});
 	});
+	/*
+	 * Make sure user has made a selection
+	 */
+	$('form#selectAnswer').submit(function() {
+		return $("input:radio[name='answer']").is(':checked');
+	});
 });
-//
-//        type: 'POST',
-//        data: { field1: "hello", field2 : "hello2"} ,
-//        contentType: 'application/json; charset=utf-8',
+
